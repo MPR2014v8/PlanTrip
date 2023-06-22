1b500296cadebb170443f85389dcf8ed2e2cc33b
blacksnow <domezaza48@gmail.com>
2023-06-22 21:47:05 +0700

    update model to admin.py

--- watchlist_app/admin.py
+++ watchlist_app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from watchlist_app.models import WatchList, StreamPlatform
+from watchlist_app.models import *
 
 # Register your models here.
 admin.site.register(WatchList)
 admin.site.register(StreamPlatform)
+admin.site.register(BusinessType)
+admin.site.register(BusinessPlace)
+admin.site.register(Trip)
